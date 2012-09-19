8c08f748456a7cb6d158f8b527ed79c334c4644c
Boris Lipin <Lonely.Ruyk@mail.ru>
2012-09-19 20:49:29 +0400

    initial commit

---
+++ get_open.c
@@ -0,0 +1,87 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/ptrace.h>
+#include <sys/reg.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+
+#include <limits.h>
+
+void
+get_open_args(pid_t pid)
+{
+	struct user_regs_struct regs;
+	unsigned char path[NAME_MAX + 1];
+	int i;
+
+	if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) == -1)
+		printf("ptrace getregs error\n");
+	i = 0;
+	while (1) {
+		unsigned int tmp;
+		char *cp;
+		int j;
+
+		errno = 0;
+		tmp = ptrace(PTRACE_PEEKTEXT, pid, regs.ebx + i, NULL);
+		if (tmp == -1 && errno != 0)
+			perror("something bad happened:");
+		cp =(char *) &tmp;
+		for (j = 0; j < 4; j++) {
+			path[i++] = cp[j];
+			if (cp[j] == '\0')
+				goto finalize;
+			if (i > NAME_MAX) {
+				printf("error, can't find \\0\n");
+				exit(1);
+			}
+		}
+	}
+finalize:
+	printf("open (\"%s\", %.8x)\n", path, (int)regs.ecx);
+}
+
+int
+main()
+{
+	int pid, status;
+	int insyscall;
+	
+        pid = fork();
+
+        switch (pid) {
+        case 0:
+                ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+                execl("/bin/ls", "ls", NULL);
+        case -1: 
+                perror("fork ");
+                exit(1);
+        default:
+                break;
+        }
+
+	insyscall = 0;
+	while (1) {
+		wait(&status);
+		//is child exited
+		if (WIFEXITED(status))
+			break;
+
+		if (insyscall == 1) {
+			insyscall = 0;
+		} else {
+			int eax;
+			eax = ptrace(PTRACE_PEEKUSER, pid, 4 * ORIG_EAX, NULL);
+			if (eax == __NR_open)
+				get_open_args(pid);
+			insyscall = 1;
+		}
+		ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+	}
+	return 0;
+}
